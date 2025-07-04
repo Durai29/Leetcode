a741ae458688181f8b00bc69c855b3dd29812039
Durai29 <muruga6129@gmail.com>
2025-07-04 11:15:31 +0530

    Leetcode : 125

--- IsPalindrome.py
+++ IsPalindrome.py
@@ -1,4 +1,23 @@
-def isPalindrome(s):                                           # Leetcode : 125
+def isPalindrome1(s):                                           # Leetcode : 125
     s = (''.join(c for c in s if c.isalnum())).lower()         # extract only the alpha and num from s and make it lowercase
     return s == s[::-1]                                        # returns true if it's a palindrome
-print(isPalindrome("0P"))
+
+def isPalindrome2(s):    # Leetcode : 125
+    # s = ''.join([i for i in s.lower() if i.isalnum()])    # change the string to lowercase and only append alphabet or numbers to the list
+    # return s == s[::-1]                                   # convert the list to string and return True if it's a palindrome
+    
+
+    l , r = 0 , len(s)-1                        # Two pointers
+    while l < r:                                # while L is less than R ( Strictly )
+        while l < r and not s[l].isalnum():     # use this loop to find a char which is alphabet or number from left
+            l += 1
+        while l < r and not s[r].isalnum():     # use this loop to find a char which is alphabet or number form right
+            r -= 1
+        if s[l].lower() != s[r].lower():        # check if both are not same
+            return False                        # if so return False
+        l += 1                                  # then increment both L and R
+        r -= 1
+
+    return True                                 # return True if the loop has executed successfully
+
+print(isPalindrome2("0P"))
