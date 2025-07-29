ce93b1425c5cc39f89e97b677b8c915c8ffed539
Durai29 <muruga6129@gmail.com>
2025-07-29 17:45:35 +0530

    Revised Leetcode : 22

--- Python/GenerateParantheses.py
+++ Python/GenerateParantheses.py
@@ -16,4 +16,6 @@ def generateParantheses(n):                                     # Leetcode : 22
     return result                                               # return the result
         
 print(generateParantheses(2))
-# Revised today ( 21 Jul 2025 )
+# Revised today ( 21 JUL 2025 )
+
+# Revised today ( 29 JUL 2025 )
