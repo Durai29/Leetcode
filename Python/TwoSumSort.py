73349018cc5a682ec0407e41acd50b0e1ec75a51
Durai29 <muruga6129@gmail.com>
2025-08-20 10:20:14 +0530

    Revised Leetcode : 167

--- Python/TwoSumSort.py
+++ Python/TwoSumSort.py
@@ -10,3 +10,21 @@ def twoSumSort(numbers, target):                                # Leetcode : 167
             return [l+1, r+1]
     
 print(twoSumSort([2,7,11,15],9))    
+
+def twoSum(numbers, target):                                    # Leetcode : 167
+    left = 0                                                    # Define left and right pointers    
+    right = len(numbers)-1
+
+    while left < right:                                         # use the while loop with condition left < right 
+        s = numbers[left] + numbers[right]                      # usually for binary search we use left <= right
+        if s == target:                                         # Here we use < only to avoid choosing the same element                    
+            return [left+1, right+1]                            # so if s equals target return the values of left + 1  and right + 1
+        elif s < target:                                        # if s is lesser, increment target by 1
+            left+=1
+        elif s > target:                                        # if s is greater, decrement the target by 1
+            right-=1
+    
+print(twoSum([2,7,11,15],9))
+print(twoSum([5,25,75],100))
+
+# Revised Today ( 20 AUG 2025 )
