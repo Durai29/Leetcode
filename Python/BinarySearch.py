a76507a11a7bfede5979a60db3b23d9bae10a45e
Durai <muruga6129@gmail.com>
2025-09-05 19:25:55 +0530

    Revised Leetcode : 704

--- Python/BinarySearch.py
+++ Python/BinarySearch.py
@@ -1,13 +1,15 @@
-def Search(nums, target):
+def Search(nums, target):           # Leetcode : 704
     left, right = 0, len(nums)-1    # here -1 is made to avoid the index out of bound error.
     while(left<=right):             # <= symbol is used, to include last element for search. Try < with the same list given and the target as 9 ( it would return -1 ) 
-        mid = (left+right)//2
-        if nums[mid]==target:
+        mid = (left+right)//2       # find the mid index between left and right index
+        if nums[mid]==target:       # if target resides in that mid index, return mid
             return mid
-        elif nums[mid]<target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return -1
+        elif nums[mid]<target:      # elif the number in mid index lesser than target
+            left = mid + 1          # increment left pointer, coz the array in sorted in ascending order
+        else:                       # else, if the mid value is greater than target
+            right = mid - 1         # decrement the right pointer
+    return -1                       # if target not found in the array, return -1
 nums = [1,2,3,4,5,6,7,8,9]
 print(Search(nums, 9))
+
+# Revised Today (5 SEP 2025)
