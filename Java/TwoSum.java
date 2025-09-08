6005e337f869fa42d60320f62888dc0c07d29869
Durai29 <muruga6129@gmail.com>
2025-09-08 17:02:10 +0530

    Java Leetcode : 1

---
+++ Java/TwoSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class TwoSum {                                                       // Leetcode : 1
+    public static int[] twoSum(int[] nums, int target){                     // twoSum method definition
+        Map<Integer,Integer> map = new HashMap<>();                         // used HashMap interface to store key : values pairs of num and it's index.
+        for(int i=0; i < nums.length; i++){                                 // used a for loop to iterate the given array
+            int complement = target - nums[i];                              // find the complement or the number that we need to find in the array.
+            if(map.containsKey(complement)){                                // check if the complement is present inside the hashmap
+                return new int[]{i,map.get(complement)};                    // if so return both the indices
+            }
+            map.put(nums[i], i);                                            // add the number and it's index to the hashmap
+        }
+        return new int[]{};                                                 // if the pairs or not found return empty array object.
+    }
+
+    public static void main(String args[]){
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}
