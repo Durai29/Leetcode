5dcc6a20c0a3bff5fed2fa525f5a736ed8ed269b
Durai29 <muruga6129@gmail.com>
2025-09-09 14:54:23 +0530

    Leetcode : 258 & Leetcode : 202

---
+++ Java/IsHappy.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class IsHappy {
+    public static boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        int rem = 0;
+        int sum = 0;
+        while(n != 1){
+            if(set.contains(n)) return false;
+            set.add(n);
+            while(n != 0){
+                rem = n % 10;
+                sum += (int) Math.pow(rem, 2);
+                n /= 10;
+            }
+            n = sum;
+            sum = 0;
+        }
+        return true;
+    }
+
+    public static void main(String arg[]){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter: ");
+        int n = scn.nextInt();
+        // int n = 7;
+        System.out.println(isHappy(n));
+        scn.close();
+    }
+}
