2addcec7b7b4b3b863dcc3bf9b3e2c0123b1985c
Durai29 <muruga6129@gmail.com>
2025-09-14 22:45:23 +0530

    Leetcode : 217

---
+++ Java/ContainsDuplicate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> seen = new HashSet<>();
+        for(int num : nums){
+            if(seen.contains(num)){
+                return true;
+            }else{
+                seen.add(num);
+            }
+        }
+        return false;
+    }
+}
