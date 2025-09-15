32ea67c01def0e9ce52d9f81e76904c187308fcd
Durai <muruga6129@gmail.com>
2025-09-15 16:42:13 +0530

    Leetcode : 121

---
+++ Java/MaxProfit.java
@@ -0,0 +1,12 @@
+class MaxProfit {                                       // Leetcode : 121
+    public int maxProfit(int[] prices) {
+        int min_price = Integer.MAX_VALUE;
+        int max_price = 0;
+        for(int p : prices){
+            min_price = min_price < p ? min_price : p;
+            int current_price = p - min_price;
+            max_price = max_price > current_price ? max_price : current_price;
+        }
+        return max_price;
+    }
+}
