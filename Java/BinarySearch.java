d5d27a7308338efafaafa3d0b44cfb28f33f9e40
Durai29 <muruga6129@gmail.com>
2025-09-18 23:19:49 +0530

    Leetcode : 704

--- Java/BinarySearch.java
+++ Java/BinarySearch.java
@@ -5,8 +5,8 @@ public class BinarySearch {
         while(left <= right){
             int mid = (left + right)/2;
             if(nums[mid] == target) return mid;
-            else if(nums[mid] < target) left++;
-            else right--;
+            else if(nums[mid] < target) left = mid+1;
+            else right=mid-1;
         }
         return -1;
     }
