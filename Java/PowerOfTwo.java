5a98b44f162152124b1975b60661d30f8d0c9484
Durai <muruga6129@gmail.com>
2025-09-19 21:06:18 +0530

    Revised Leetcode : 231

--- Java/PowerOfTwo.java
+++ Java/PowerOfTwo.java
@@ -1,5 +1,8 @@
 public class PowerOfTwo {                           // Leetcode : 231
     public static boolean isPowerOfTwo(int n){
+        if(n<=0) return false;
         return (n & (n-1)) == 0;
     }
 }
+
+// Revised Today (19 SEP 2025)
