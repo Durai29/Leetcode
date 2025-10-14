a495eb844cf837363a87b5f2b8c4d1ac0dcad3c0
Durai29 <muruga6129@gmail.com>
2025-10-14 18:14:07 +0530

    Leetcode : 292

---
+++ Java/NimGame.java
@@ -0,0 +1,11 @@
+public class NimGame {
+    public boolean canWinNim(int n) {
+        if(n < 4) return true;
+        else if(n % 4 == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
