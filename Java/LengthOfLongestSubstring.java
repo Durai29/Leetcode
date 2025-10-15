b3358e1822812e5fc02c5fed1f19401d66ff5781
Durai29 <muruga6129@gmail.com>
2025-10-16 01:00:28 +0530

    Leetcode : 03

---
+++ Java/LengthOfLongestSubstring.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LengthOfLongestSubstring {
+    public int lengthOfLongestSubstring(String s) {
+        char[] s_char = s.toCharArray();
+        int count = 0;
+        Map<Character,Integer> pair = new HashMap<>();
+        int left = 0;
+        for(int right = 0; right < s.length(); right++){
+            char c = s_char[right];
+            if(pair.containsKey(c)){
+                left = Math.max(left, pair.get(c)+1);
+            }
+            pair.put(c,right);
+            count = Math.max(count, right-left+1);
+        }
+        return count;
+    }
+}
